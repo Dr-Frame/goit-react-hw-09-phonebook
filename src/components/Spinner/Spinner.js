940efab250baeba5df302c1efd833b7191caa592
Vlad Dr.Frame <remenjuk2010@gmail.com>
2021-05-27 20:50:18 +0300

    minor fixes

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -8,5 +8,5 @@ const override = css`
 `;
 
 export default function Spinner() {
-  return <MoonLoader color="#0575e6" size="60" css={override}></MoonLoader>;
+  return <MoonLoader color="#0575e6" size="60px" css={override}></MoonLoader>;
 }
